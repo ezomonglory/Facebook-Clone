8f0bd0ab06648ba43d3cc90a253c37213e28785f
Ezomon Glory <ezomonglory01@gmail.com>
2022-07-27 22:50:45 +0100

    received data from facebook

--- components/SideBar.js
+++ components/SideBar.js
@@ -1,39 +1,38 @@
-import React from 'react'
-import { useSession } from "next-auth/react"
-import SideBarRow from './SideBarRow'
+import React from "react";
+import { useSession } from "next-auth/react";
+import SideBarRow from "./SideBarRow";
 import Image from "next/image";
-
 
 import {
 	DesktopComputerIcon,
-    ClockIcon,
-    CalendarIcon,
+	ClockIcon,
+	CalendarIcon,
 	UsersIcon,
 } from "@heroicons/react/solid";
 
 import {
-    UserGroupIcon,
+	UserGroupIcon,
 	ChevronDownIcon,
 	ShoppingBagIcon,
 } from "@heroicons/react/outline";
 
 function SideBar() {
-    const {session, loading} = useSession()
+	const { data: session } = useSession();
 
-  return (
-      <div className='p-2 mt-5 sm:inline max-w-[600px] xl:min-w-[300px] hidden'>
-        {/* <Image src="/image/fb.png" width={30} height={30}/> */}
-        <SideBarRow src="/image/fb.png" title="Glory Ezomon" />
-        {/* <SideBarRow src={session.user.image} title={session.user.name} /> */}
-        <SideBarRow Icon={UsersIcon} title="Friends" />
-        <SideBarRow Icon={UserGroupIcon} title="Groups" />
-        <SideBarRow Icon={ShoppingBagIcon} title="MarketPlace" />
-        <SideBarRow Icon={DesktopComputerIcon} title="Watch" />
-        <SideBarRow Icon={CalendarIcon} title="Events" />
-        <SideBarRow Icon={ClockIcon} title="Memories" />
-        <SideBarRow Icon={ChevronDownIcon} title="See More" />
-    </div>
-  )
+	return (
+		<div className='p-2 mt-5 sm:inline max-w-[600px] xl:min-w-[300px] hidden'>
+			{/* <Image src="/image/" width={30} height={30}/> */}
+			{/* <SideBarRow src="/image/fb.png" title="Glory Ezomon" /> */}
+			<SideBarRow src={session.user.image} title={session.user.name} />
+			<SideBarRow Icon={UsersIcon} title='Friends' />
+			<SideBarRow Icon={UserGroupIcon} title='Groups' />
+			<SideBarRow Icon={ShoppingBagIcon} title='MarketPlace' />
+			<SideBarRow Icon={DesktopComputerIcon} title='Watch' />
+			<SideBarRow Icon={CalendarIcon} title='Events' />
+			<SideBarRow Icon={ClockIcon} title='Memories' />
+			<SideBarRow Icon={ChevronDownIcon} title='See More' />
+		</div>
+	);
 }
 
-export default SideBar
+export default SideBar;
