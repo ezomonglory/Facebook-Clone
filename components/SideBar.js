5d6e5be3ebce2e62bb64ef6b47f1538a3514e035
Ezomon Glory <ezomonglory01@gmail.com>
2022-07-27 22:27:26 +0100

    created SideBar

---
+++ components/SideBar.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useSession } from "next-auth/react"
+import SideBarRow from './SideBarRow'
+import Image from "next/image";
+
+
+import {
+	DesktopComputerIcon,
+    ClockIcon,
+    CalendarIcon,
+	UsersIcon,
+} from "@heroicons/react/solid";
+
+import {
+    UserGroupIcon,
+	ChevronDownIcon,
+	ShoppingBagIcon,
+} from "@heroicons/react/outline";
+
+function SideBar() {
+    const {session, loading} = useSession()
+
+  return (
+      <div className='p-2 mt-5 sm:inline max-w-[600px] xl:min-w-[300px] hidden'>
+        {/* <Image src="/image/fb.png" width={30} height={30}/> */}
+        <SideBarRow src="/image/fb.png" title="Glory Ezomon" />
+        {/* <SideBarRow src={session.user.image} title={session.user.name} /> */}
+        <SideBarRow Icon={UsersIcon} title="Friends" />
+        <SideBarRow Icon={UserGroupIcon} title="Groups" />
+        <SideBarRow Icon={ShoppingBagIcon} title="MarketPlace" />
+        <SideBarRow Icon={DesktopComputerIcon} title="Watch" />
+        <SideBarRow Icon={CalendarIcon} title="Events" />
+        <SideBarRow Icon={ClockIcon} title="Memories" />
+        <SideBarRow Icon={ChevronDownIcon} title="See More" />
+    </div>
+  )
+}
+
+export default SideBar
--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home({ session }) {
 			<Header />
 			<main className="flex">
 				{/* sideBar */}
-				{/* <SideBar /> */}
+				<SideBar />
 				{/* Feed that is the center */}
 				{/* <Feed /> */}
 				{/* widget */}
