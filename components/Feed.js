cae0f7c4ef0b84cd424a43d93ee59db36d2343f1
ezomonglory <96381869+ezomonglory@users.noreply.github.com>
2022-07-27 22:56:51 +0100

    Update Feed.js

--- components/Feed.js
+++ components/Feed.js
@@ -5,7 +5,7 @@ import Stories from './Stories'
 
 function Feed() {
   return (
-    <div className='flex-grow flex h-screen pb-44  pt-6 mr-4 xl:mr-40 overflow-y-auto overflow-x-none' >
+    <div className='flex-grow flex h-screen pb-44  pt-6 mr-4 xl:mr-40 overflow-y-auto overflow-x-auto' >
         <div className='mx-auto max-w-full md:max-w-lg lg:max-w-2xl min-w-full'>
             {/* stories */}
             <Stories />
