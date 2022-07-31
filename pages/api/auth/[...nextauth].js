2195b51ff7bdbe7697ec7380b858a5944b3cb670
Ezomon Glory <ezomonglory01@gmail.com>
2022-07-31 18:40:02 +0100

    added nextauth-secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,14 +1,15 @@
 import NextAuth from "next-auth"
 import FacebookProvider from 'next-auth/providers/facebook'
 
-export const authOptions= {
+export const  authOptions= {
   providers: [
     // OAuth authentication providers...
     FacebookProvider({
       clientId: process.env.FACEBOOK_ID,
       clientSecret: process.env.FACEBOOK_SECRET
     }),
-  ]
+  ],
+  secret:process.env.NEXTAUTH_SECRET
 }
 
 
